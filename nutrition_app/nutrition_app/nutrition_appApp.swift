aa8cb7ab0e3c33a7226e5c0a4d5419d2211858e6
EdiZhang123 <edizhang05@gmail.com>
2026-04-19 10:30:20 -0700

    Initialize Xcode project

---
+++ nutrition_app/nutrition_app/nutrition_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  nutrition_appApp.swift
+//  nutrition_app
+//
+//  Created by Edi Zhang on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct nutrition_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
